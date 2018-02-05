be6b40a2567533b661c7055d2fc9c8c521fb99db
Andy Maskiell <maskiella@gmail.com>
2018-02-05 17:45:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time = Time.getlocal.hour
+    time = Time.now.hour
 
     if time >= 0 && time <= 12
       resp.write "Good Morning!"
